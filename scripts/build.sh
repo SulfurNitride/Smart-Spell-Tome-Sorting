55710a83fdf2ed762e1fde8be079506da5792e7c
SulfurNitride <182011199+SulfurNitride@users.noreply.github.com>
2026-08-23 15:51:58 -0500

    Revert "Simplify build infrastructure"
    This reverts commit 153155aa304d7e78fc8eecd0ffa771ec706c30ab.

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cmake \
+    -S /work \
+    -B /work/build \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" \
+    -DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=/work/cmake/windows-msvc-clang.cmake \
+    -DVCPKG_APPLOCAL_DEPS=OFF \
+    -DVCPKG_OVERLAY_TRIPLETS=/work/cmake/triplets \
+    -DVCPKG_TARGET_TRIPLET=x64-windows-clang-linux \
+    -DVCPKG_HOST_TRIPLET=x64-linux
+
+cmake --build /work/build --target package_mod --parallel
